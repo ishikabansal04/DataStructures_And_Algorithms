0fef59aefd03a0ca28291a819877097f965f692c
ishikabansal04 <ikabansal04@gmail.com>
2021-01-06 23:34:14 +0530

    Adding codes for graphs

--- Graphs/Snakes_Ladders.cpp
+++ Graphs/Snakes_Ladders.cpp
@@ -5,15 +5,18 @@
 #include<climits>
 using namespace std;
 
-    void Snakes_Ladders(vector<vector<int>>&ladders){
-        vector<int>distance(31, 0);
+    void Snakes_Ladders(vector<vector<int>>&ladders, vector<vector<int>>&snakes){
+        vector<int>distance(101, 0);
         for(int i=0;i<ladders.size();i++){
             distance[ladders[i][0]]+=(ladders[i][1]-ladders[i][0]);
+        }
+        for(int i=0;i<snakes.size();i++){
+            distance[snakes[i][0]]+=(snakes[i][1]- snakes[i][0]);
         }
         int count=0;
         queue<int>que;
         que.push(1);
-        vector<int>moves(31,INT_MAX);
+        vector<int>moves(101,INT_MAX);
         bool res=false;
         moves[1]=0;
         for(int i=2;i<7;i++){
@@ -23,7 +26,7 @@ using namespace std;
 
             int rn=que.front();
             que.pop();
-            if(rn==30){
+            if(rn==100){
                 res=true;
                 break;
             }
@@ -31,7 +34,7 @@ using namespace std;
             for(int i=1;i<=6;i++){
                 int x=rn+i;
                 x=x+ distance[x];
-                if(x<=30 && x>=1){
+                if(x<=100 && x>=1){
                     if(moves[x]>=moves[rn]+1){
                         moves[x]=moves[rn]+1;
                         que.push(x);
@@ -43,21 +46,22 @@ using namespace std;
             cout<<-1<<endl;
         }
         else{
-            cout<<moves[30]<<endl;
+            cout<<moves[100]<<endl;
         }
     }
 
 
 int main(){
-    int tc;
-    cin>>tc;
-    for(int t=0;t<tc;t++){
-        int nl;
-        cin>>nl;
-        vector<vector<int>>ladders(nl, vector<int>(2));
-        for(int i=0;i<nl;i++){
-            cin>>ladders[i][0]>>ladders[i][1];
-        } 
-        Snakes_Ladders(ladders);
+    int nl, ns;
+    cin>>nl;
+    vector<vector<int>>ladders(nl, vector<int>(2));
+    for(int i=0;i<nl;i++){
+        cin>>ladders[i][0]>>ladders[i][1];
     }
+    cin>>ns;
+    vector<vector<int>>snakes(ns, vector<int>(2));
+    for(int i=0;i<ns;i++){
+        cin>>snakes[i][0]>>snakes[i][1];
+    }
+    Snakes_Ladders(ladders, snakes);
 }
--- Graphs/q.cpp
+++ Graphs/q.cpp
@@ -9,92 +9,50 @@
 #include<string>
 using namespace std;
 
-#define mp make_pair
-#define INFI 10e8
-#define INF 10e7
-
-typedef pair<int, int> pi;
-typedef vector<int> vi;
-typedef vector<bool> vb;
-
-
-class triplet{
-public:
-    int vtx;
-    string path;
-    int distance;   
-
-    triplet(){
-        this-> vtx = -1;
-        this-> path = "";
-        this-> distance = INT_MAX;
+pair<int, int> POC(int sr, int sc, int k, vector<vector<int>>&dir, int n, int idx, unordered_map<pair<int, int>, bool>corners){
+    if(k==0){
+        return make_pair(sr, sc);
     }
-
-    triplet(int vtx, string path, int distance){
-        this-> vtx = vtx;
-        this-> path = path;
-        this-> distance = distance;
+    pair<int,int>p1(sr, sc);
+    if(corners.count(p1)>0){
+        return p1;
     }
-
-};
-
-struct compare{
-    bool operator()(const triplet &t1, const triplet &t2){
-        return t1.distance > t2.distance;
+    int x=sr, y=sc;
+    for(int i=0;x<=n && x>=0 && y<=n && y>=0;i++){
+        x=sr+i*dir[idx][0];
+        y=sc+i*dir[idx][1];
     }
-};
-
-class Graph{
-public:
-    unordered_map<int, list<pi> > gmap;
-
-    void addEdge(int src, int dest, int distance, bool bidir = true){
-        gmap[src].push_back(mp(dest, distance));
-
-        if(bidir){
-            gmap[dest].push_back(mp(src, distance));
-        }
+    idx=idx+1;
+    if(idx==3){
+        idx=0;
     }
-    // * Triplet class ==>  {vtx, "path", distance}
-
-    void shortest_distance(int V, int E, Graph g, int src){
-        priority_queue<triplet, vector<triplet>, compare> pq;
-        vb visited(V, false);
-        triplet src_triplet(src, to_string(src), 0);
-        pq.push(src_triplet);
-        while(!pq.empty()){
-            triplet top = pq.top();
-            pq.pop();
-            if(visited[top.vtx]){
-                continue;
-            }
-            visited[top.vtx] = true;
-            cout << top.vtx << " via " << top.path << " @ " << top.distance << '\n';
-            for(auto neighbour : gmap[top.vtx]){
-                    if(!visited[neighbour.first]){
-                        int child = neighbour.first;
-                        int child_from_vtx = neighbour.second;
-                        int child_from_src = top.distance + neighbour.second;
-                        triplet child_triplet(child, top.path + to_string(child), child_from_src);
-                        cout<<child<<"      "<<top.path + to_string(child)<<"      "<<child_from_src<<endl;
-                        pq.push(child_triplet);
-                   }
-            }
-        }
-    }   
-};
-
+    pair<int, int>smallpair= POC(x, y, k-1, dir, n, idx, corners);
+    return smallpair;
+}
 
 int main(){
     std::ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    Graph g;
-    int V, E, u, v, d, start;
-    cin >> V >> E;
-    for(int i = 0; i < E; i++){
-        cin >> u >> v >> d;
-        g.addEdge(u, v, d);
+    int tc;
+    cin>>tc;
+    for(int t=0;t<tc;t++){
+        int n,k;
+        cin>>n>>k;
+        int x,y;
+        cin>>x>>y;
+        
+        unordered_map<pair<int, int>, bool> corners;
+        pair<int, int> p1(0, n); 
+        pair<int, int> p2(n, 0);  
+        pair<int, int> p3(0, 0); 
+        pair<int, int> p4(n, n); 
+
+        corners[p1] = true; 
+        corners[p2] = true; 
+        corners[p3] = true; 
+        corners[p4] = true;
+
+        vector<vector<int>>dir={{1,1}, {1,-1},{-1, -1}, {-1, 1}};
+        pair<int, int> mypair= POC(x, y, k, dir, n, 0, corners);
+        cout<<mypair.first<<" "<<mypair.second<<endl;
     }
-    cin >> start;
-    g.shortest_distance(V, E, g, start);
 }
