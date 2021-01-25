21efce3e069dab2d7839a773fec0deaeb3cf77cf
ishikabansal04 <ikabansal04@gmail.com>
2021-01-25 23:06:30 +0530

    Adding codes for hashmaps & heaps

---
+++ Hashmaps Heaps/LongestSubarrWithSumDivByK.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+    int maxlen=0;
+    int currlen=0;
+    for(int i=0;i<)
+}
