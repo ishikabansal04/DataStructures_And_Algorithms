dca1e7b9159524c68be3be3ce2ec59b0255b6104
ishikabansal04 <ikabansal04@gmail.com>
2021-01-30 23:28:27 +0530

    Modified Codes

--- Hashmaps Heaps/q.cpp
+++ Hashmaps Heaps/q.cpp
@@ -1,31 +1,67 @@
 #include<iostream>
+#include<unordered_map>
+#include<list>
 #include<vector>
+#include<stack>
+#include<queue>
+#include<climits>
 #include<string>
+#include<algorithm>
+#include<utility>
 using namespace std;
+
+#define mp make_pair
+#define INFI 10e8
+#define INF 10e7
+
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef long long ll;
+
 int main(){
-    long long tc;
-    cin>>tc;
-    for(long long t=0;t<tc;t++){
-        long long val;
-        cin>>val;
-        if(val!=1 && val%2!=0){
-            cout<<"YES"<<endl;
+    unordered_map<string, char> my_map;
+    unordered_map<char, int> visited;
+    string s1 = "", s2 = "", temp = "";
+    getline(cin, s1);
+    getline(cin, s2);
+    bool rhyme = true;
+    int j = 0;
+    for(int i = 0; i < s2.size(); i++){
+        if(s2[i] == ' '){
+            if(my_map.count(temp) > 0){
+
+                if(my_map[temp] != s1[j]){
+                    cout << "false" << '\n';
+                    rhyme = false;
+                    break;
+                }
+            }
+            else{
+                if(visited.count(s1[j]) == 0){
+                    my_map[temp] = s1[j];
+                    visited[s1[j]] = 1;
+                }
+                else{
+                    cout << false << '\n';
+                    rhyme = false;
+                    break;
+                }
+            }
+            temp = "";
+            j++;
         }
         else{
-            bool flag=false;
-    
-        for(long long i=2;i*i<=val && i%2!=0; i++){
-            if(val%i==0){
-                cout<<i<<endl;
-                cout<<"YES"<<endl;
-                flag=true;
-                break;
-            }
+            temp += s2[i];
         }
-        if(!flag){
-            cout<<"NO"<<endl;
-        }
-        }
-        
     }
+
+    if(rhyme){
+        if((my_map.count(temp) > 0 && my_map[temp] == s1[j]) || (my_map.count(temp) == 0 && visited.count(s1[j]) == 0)){ 
+            cout << "true" << '\n';
+        }
+        else{
+            cout << "false" << '\n';
+        }
+    }  
 }
