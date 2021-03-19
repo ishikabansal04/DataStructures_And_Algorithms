450188e97c2cc3c3f6d426849fac3128e5103c65
ishikabansal04 <ikabansal04@gmail.com>
2021-03-19 23:26:58 +0530

    Adding codes for dynamic programming

---
+++ DP/CountPallindromicSubsequences.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+
+    string str2="";
+    for(int i=0; i<str.length(); i++){
+        str2= str[i]+ str2;
+    }
+    vector<vector<int>>dp(str.length()+1, vector<int>(str.length()+1, 0));
+    for(int i=0; i<=str.length(); i++){
+        for(int j=0; j<=str.length(); j++){
+            if(i==j){
+                dp[i][j]=1;
+            }
+            else if(str[i-1]== str[j-1]){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + 1;
+            }
+            else{
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+            }
+        }
+    }
+    cout<<dp[str.length()][str.length()];
+    for(int i=0; i<=str.length(); i++){
+        for(int j=0; j<=str.length(); j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
